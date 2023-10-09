34d822a5b6ab81e0d96b8208e628e6cdd9064335
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-10-09 17:23:25 +0500

    Image being shown on screen

--- app/src/main/java/com/example/storageapp/ImageSelectAndSendActivity.java
+++ app/src/main/java/com/example/storageapp/ImageSelectAndSendActivity.java
@@ -1,14 +1,13 @@
 package com.example.storageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
@@ -17,7 +16,8 @@ public class ImageSelectAndSendActivity extends AppCompatActivity {
     private static final int SELECT_IMAGE = 1;
     private static final long THRESHOLD_SIZE = 1 * 1024 * 1024;  // 1 MB
     Button selectImageButton, acceptSendButton, rejectButton;
-    TextView errorMessageTextView;  // New TextView for error messages
+    TextView errorMessageTextView;
+    ImageView selectedImageView;
     Uri selectedImageUri;
 
     @Override
@@ -28,7 +28,8 @@ public class ImageSelectAndSendActivity extends AppCompatActivity {
         selectImageButton = findViewById(R.id.selectImageButton);
         acceptSendButton = findViewById(R.id.acceptSendButton);
         rejectButton = findViewById(R.id.rejectButton);
-        errorMessageTextView = findViewById(R.id.errorMessageTextView);  // Initialize the TextView
+        errorMessageTextView = findViewById(R.id.errorMessageTextView);
+        selectedImageView = findViewById(R.id.selectedImageView);
 
         selectImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +48,6 @@ public class ImageSelectAndSendActivity extends AppCompatActivity {
                 sendIntent.setType("image/*");
                 startActivity(Intent.createChooser(sendIntent, "Send Image"));
 
-                // Log the details
                 String logEntry = "Sent image of size: " + getSizeFromUri(selectedImageUri) + " bytes at " + System.currentTimeMillis();
                 logDetails(logEntry);
             }
@@ -58,7 +58,8 @@ public class ImageSelectAndSendActivity extends AppCompatActivity {
             public void onClick(View v) {
                 acceptSendButton.setVisibility(View.GONE);
                 rejectButton.setVisibility(View.GONE);
-                errorMessageTextView.setVisibility(View.GONE);  // Hide the error message
+                errorMessageTextView.setVisibility(View.GONE);
+                selectedImageView.setImageDrawable(null);  // Remove the displayed image
             }
         });
     }
@@ -69,14 +70,15 @@ public class ImageSelectAndSendActivity extends AppCompatActivity {
         if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK && data != null) {
             selectedImageUri = data.getData();
             if (selectedImageUri != null) {
+                selectedImageView.setImageURI(selectedImageUri);
                 long imageSize = getSizeFromUri(selectedImageUri);
                 if (imageSize <= THRESHOLD_SIZE) {
                     acceptSendButton.setVisibility(View.VISIBLE);
                     rejectButton.setVisibility(View.VISIBLE);
-                    errorMessageTextView.setVisibility(View.GONE);  // Hide the error message
+                    errorMessageTextView.setVisibility(View.GONE);
                 } else {
-                    errorMessageTextView.setText("File size is too large!");  // Set the error message
-                    errorMessageTextView.setVisibility(View.VISIBLE);  // Show the error message
+                    errorMessageTextView.setText("File size is too large!");
+                    errorMessageTextView.setVisibility(View.VISIBLE);
                 }
             }
         }
